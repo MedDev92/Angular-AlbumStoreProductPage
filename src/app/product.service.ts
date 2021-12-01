dafcbdb6680b64cd9ad3c84deea9faa82577d47a
mmissaou <missaou.mohamed@gmail.com>
2021-12-01 16:03:14 +0100

    Creating a Service to Get Album Data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs/Observable';
-import { Album } from './album';
 import 'rxjs/add/operator/map';
+import * as myJson from 'your-jsonn-dir/your-json-file.json';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
   constructor(private _http: HttpClient) {}
 
-  getAlbum (id: number) : Observable<Album>{
-    return this._http.get(this._albumUrl)
-      ;
+  getAlbum (id: number) {
+    return this._http.get(this._albumUrl).map((response)=> response);
   }
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -4,5 +4,10 @@ interface NodeModule {
   id: string;
 }
 
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
+
 // declare the type for since, which is a global name added by the jasmine2-custom-message plugin
 declare var since: any;
